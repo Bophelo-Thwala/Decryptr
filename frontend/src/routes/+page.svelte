fa312024e0427809aafec13ec7a3dda6421d2d85
Bophelo-Thwala <bophelokal@gmail.com>
2025-08-27 01:35:44 +0200

    Fixed Build Issue #3

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,18 +3,10 @@
 <script>
     import { goto } from "$app/navigation";
     import { onMount } from "svelte";
-    import { writable } from "svelte/store";
-    import { PUBLIC_PORT } from "$env/static/public";
-
-    const PORT = writable("");
-
-    console.log("\nRunning SvelteKit on port", PORT);
 
     onMount(() => {
         setTimeout(() => {
             console.log("Redirecting to home page...");
-            PORT.set(PUBLIC_PORT);
-            console.log("SvelteKit on port " + $PORT);
             goto('/home');
         }, 3000); // Redirect after 3 seconds
     });
