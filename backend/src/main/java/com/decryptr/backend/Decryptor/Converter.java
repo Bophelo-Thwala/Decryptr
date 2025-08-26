498ff1b0d700f1af1ff524b084690a139ac67e3d
Bophelo-Thwala <bophelokal@gmail.com>
2025-08-26 19:31:18 +0200

    Deleted and Recreated backend

--- backend/src/main/java/com/decryptr/backend/Decryptor/Converter.java
+++
@@ -1,5 +0,0 @@
-package com.decryptr.backend.Decryptor;
-
-public class Converter {
-    public static void convert() {}
-}
--- backend/src/main/java/com/decryptr/backend/Decryptor/Decrypt.java
+++
@@ -1,9 +0,0 @@
-package com.decryptr.backend.Decryptor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
-public class Decrypt {
-    public static void decrypt() {}
-}
